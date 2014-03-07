73d7d178b432b5a489d034985bd7f34ba4eba0a4
Paul Morie <pmorie@gmail.com>
2014-03-07 18:27:21 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 go-dockerclient-commit
 ======================
+
+This is a tiny test repo that recreates an issue I've been having with committing containers using
+[foo](http://github.com/fsouza-dockerclient).  The when I create an image by commiting a container
+via the library, the resulting image does not have a CMD.
+
+### Usage
+
+    go test github.com/pmorie/go-dockerclient-commit
---
+++ commit_test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"log"
+	"testing"
+
+	"github.com/fsouza/go-dockerclient"
+)
+
+func removeContainer(client *docker.Client, id string) {
+	client.RemoveContainer(docker.RemoveContainerOptions{ID: id})
+}
+
+func removeImage(client *docker.Client, name string) {
+	client.RemoveImage(name)
+}
+
+func exerciseCommit(setCmd bool, t *testing.T) {
+	testImage := "go-dc/commit-test"
+	dockerClient, err := docker.NewClient("unix:///var/run/docker.sock")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	removeImage(dockerClient, testImage)
+
+	config := docker.Config{Image: "pmorie/sti-fake", Cmd: []string{"/bin/true"}}
+	container, err := dockerClient.CreateContainer(docker.CreateContainerOptions{Name: "", Config: &config})
+	if err != nil {
+		t.Error(err.Error())
+	}
+	defer removeContainer(dockerClient, container.ID)
+
+	err = dockerClient.StartContainer(container.ID, &docker.HostConfig{})
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	exitCode, err := dockerClient.WaitContainer(container.ID)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if exitCode != 0 {
+		t.Error("Unexpected exit code")
+	}
+
+	config = docker.Config{}
+	if setCmd {
+		config.Cmd = []string{"/usr/bin/run"}
+	}
+
+	log.Printf("Commiting container with config: %+v\n", config)
+	createdImage, err := dockerClient.CommitContainer(docker.CommitContainerOptions{Container: container.ID, Repository: testImage, Run: &config})
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	log.Printf("Committed image: %+v\n", createdImage)
+
+	config = docker.Config{Image: testImage, AttachStdout: false, AttachStdin: false}
+	container, err = dockerClient.CreateContainer(docker.CreateContainerOptions{Name: "", Config: &config})
+	if err != nil {
+		t.Error(err.Error())
+	}
+	defer removeContainer(dockerClient, container.ID)
+
+	err = dockerClient.StartContainer(container.ID, &docker.HostConfig{})
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	exitCode, err = dockerClient.WaitContainer(container.ID)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if exitCode != 0 {
+		t.Error("Unexpected exit code from created image")
+	}
+
+}
+
+func TestCommitWithoutCmd(t *testing.T) {
+	exerciseCommit(false, t)
+}
+
+func TestCommitWithCmd(t *testing.T) {
+	exerciseCommit(true, t)
+}
+
+func TestThing(t *testing.T) {
+	t.Error("ugh")
+}
